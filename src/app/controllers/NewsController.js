6f2013a628a94fb25f1a3d3f054555150b2864eb
Tuan Hiep <tuanhiep@gmail.com>
2023-08-10 11:34:42 +0700

    prettier, lint-staged, husky

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,13 +1,13 @@
 class NewsController {
-  //[GET] /news
-  index(req, res) {
-    res.render("news");
-  }
+    //[GET] /news
+    index(req, res) {
+        res.render('news');
+    }
 
-  //[GET] /news/:slug
-  show(req, res) {
-    res.send("NEWS DEtail");
-  }
+    //[GET] /news/:slug
+    show(req, res) {
+        res.send('NEWS DEtail');
+    }
 }
 module.exports = new NewsController();
 // const NewController = require("./NewsController")
