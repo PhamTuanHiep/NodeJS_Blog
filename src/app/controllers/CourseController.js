cd5384d52aeb7c9fc6612a222c9eb73772c9f67c
Tuan Hiep <tuanhiep@gmail.com>
2023-08-14 21:01:52 +0700

    create course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -10,5 +10,17 @@ class CourseController {
       })
       .catch(next);
   }
+  create(req, res, next) {
+    res.render("courses/create");
+  }
+  store(req, res, next) {
+    const formData = req.body;
+    formData.image = `https://i.ytimg.com/vi/${req.body.videoId}/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCLxJbYhCQmzlNAkL1llJ_N6qL_zA`;
+    const course = new Course(formData);
+    course
+      .save()
+      .then(() => res.redirect("/"))
+      .catch((error) => {});
+  }
 }
 module.exports = new CourseController();
