f9567c3e8cbe431871f083120aac21c6012921e8
Tuan Hiep <tuanhiep@gmail.com>
2023-08-23 11:53:07 +0700

    sort columns by midleware

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const { engine } = require("express-handlebars");
 const methodOverride = require("method-override");
 
+const SortMiddleware = require("./app/middlewares/SortMiddleware");
+
 const route = require("./routes");
 const db = require("./config/db");
 
@@ -13,12 +15,14 @@ const port = 3000;
 //Connect to DB
 db.connect();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(methodOverride("_method"));
+
+app.use(SortMiddleware);
 //HTTP logger
 // app.use(morgan("combined"));
 
@@ -29,6 +33,25 @@ app.engine(
     extname: ".hbs",
     helpers: {
       sum: (a, b) => a + b,
+      sortable: (field, sort) => {
+        const sortType = field === sort.column ? sort.type : "default";
+        const icons = {
+          default: "oi oi-elevator",
+          asc: "oi oi-sort-ascending",
+          desc: "oi oi-sort-descending",
+        };
+        const types = {
+          default: "desc",
+          asc: "desc",
+          desc: "asc",
+        };
+        const icon = icons[sortType];
+        const type = types[sortType];
+
+        return `<a href="?_sort&column=${field}&type=${type}">
+        <span class="${icon}"></span>
+        </a>`;
+      },
     },
   })
 );
